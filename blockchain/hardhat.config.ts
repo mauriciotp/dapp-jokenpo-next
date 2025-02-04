b7753417e45e1278dba4a782e9baf93831206e90
mauricio <mauriciop@cpqd.com.br>
2025-02-03 23:42:25 -0300

    feat: setup hardhat config

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,32 @@
-import { HardhatUserConfig } from 'hardhat/config'
+import { HardhatUserConfig, vars } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+
+const NODE_URL = vars.get('NODE_URL')
+const SECRET = vars.get('SECRET')
+const API_KEY = vars.get('API_KEY')
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
+  defaultNetwork: 'local',
+  networks: {
+    local: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk',
+      },
+    },
+    sepolia: {
+      url: NODE_URL,
+      chainId: 11155111,
+      accounts: {
+        mnemonic: SECRET,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: API_KEY,
+  },
 }
 
 export default config
