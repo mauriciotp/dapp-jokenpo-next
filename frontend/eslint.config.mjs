ea0beb8cb46c31905e421d982d65e92c1a45b149
mauricio <mauricio.tp_@outlook.com>
2025-02-06 20:48:46 -0300

    setup frontend

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,37 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+      'plugin:tailwindcss/recommended',
+    ],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  }),
+]
+
+export default eslintConfig
